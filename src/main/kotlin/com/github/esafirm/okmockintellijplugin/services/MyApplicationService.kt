8a758313e94af4f5319909011fe755392fb79c86
GitHub Action <action@github.com>
2021-01-06 13:01:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/esafirm/okmockintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.esafirm.okmockintellijplugin.services
+
+import com.github.esafirm.okmockintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/esafirm/okmockintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/esafirm/okmockintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.esafirm.okmockintellijplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.esafirm.okmockintellijplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
